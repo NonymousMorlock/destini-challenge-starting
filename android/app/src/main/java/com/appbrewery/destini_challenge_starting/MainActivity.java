691803d4a32d614d53c823edaa2f992865c565bb
NonymousMorlock <agamspaul@gmail.com>
2025-11-26 14:53:27 +0000

    ✅ build: migrate native android/ios projects to Kotlin DSL and Gradle 9 - Deleted and regenerated android and ios directories to resolve legacy Gradle compatibility issues. - Migrated build scripts from Groovy to Kotlin DSL (.kts). - Used `--project-name foobar` to maintain the `co.something.foobar` applicationId while keeping the `foo_bar` dart package. - Restored original app icons (res/ and Assets.xcassets). - Updated AndroidManifest label to the correct human-readable app name. - Replace `FlatButton` with `TextButton`

--- android/app/src/main/java/com/appbrewery/destini_challenge_starting/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.appbrewery.destini_challenge_starting;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,4 @@
 import 'package:flutter/material.dart';
-import 'flatbutton.dart';
-
-// flatbutton has been removed from flutter, but this lesson depends on flatbutton,
-// if you type FlatButton, you will get an error saying it's deprecated and no longer a part of flutter
-// so i created a new flatbutton, so you can follow along with the challenge
-// so when you see FlatButto(m) in the code, you can simply ignore it and know
-// it's FlatButton but just one i created for this course since the original
-// no longer works
-// if you look in your lib folder, you'll also see flatbutton.dart, that is the widget i created
-// so here i've imported it, so you can use it
-// you can delete all these comments after you read them, so it doesn't distract you from the lesson
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
@@ -60,12 +49,15 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                child: FlatButtom(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    // replaces color: Colors.red,
+                    backgroundColor: Colors.red,
+                  ),
                   onPressed: () {
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                   },
-                  color: Colors.red,
                   child: const Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     'Choice 1',
@@ -82,12 +74,15 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButtom(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    // replaces color: Colors.blue,
+                    backgroundColor: Colors.blue,
+                  ),
                   onPressed: () {
                     //Choice 2 made by user.
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
                   },
-                  color: Colors.blue,
                   child: const Text(
                     //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
                     'Choice 2',
